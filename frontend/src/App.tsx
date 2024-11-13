16d95661ae98ca52d3485dcd85888806320a9f44
Benjamín Havlík <benjamin.havlik@lifeliqe.com>
2024-11-13 16:46:25 +0100

    fix: remove unnecessary things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import { ThemeProvider } from './components/theme-provider'
-import { ToastContainer, toast } from 'react-toastify'
-import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <ToastContainer position="bottom-right" autoClose={5000}/>
         <Outlet />
       </ThemeProvider>
     </>
