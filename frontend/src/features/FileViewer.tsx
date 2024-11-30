d5229dd7d61d360c64fda46a9d208b9a621af7d4
Benjamín Havlík <benjamin.havlik@lifeliqe.com>
2024-11-30 15:34:40 +0100

    feat: button to see files in a bigger format

--- frontend/src/features/FileViewer.tsx
+++ frontend/src/features/FileViewer.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
 type FileViewerProps = {
@@ -8,22 +9,27 @@ type FileViewerProps = {
     onClose: () => void;
 };
 
-// TODO: upravit aby obrazky ukazovalo mensie a bez scrollbarov
-
 export default function FileViewer({ fileUrl, fileType, fileName, open, onClose }: FileViewerProps) {
     return (
         <Dialog open={open} onOpenChange={(isOpen) => !isOpen && onClose()}>
-            <DialogContent className="w-full max-w-4xl max-h-[90vh] h-auto p-4 rounded-md overflow-hidden">
+            <DialogContent className="max-w-4xl max-h-[90vh] h-auto p-4 rounded-md overflow-hidden">
                 <DialogHeader className="flex justify-between items-center">
                     <DialogTitle>{fileName}</DialogTitle>
                 </DialogHeader>
-                <div className="flex justify-center items-center w-full h-full mt-4 overflow-hidden">
+                <div className="flex justify-center">
+                    <Button asChild variant="outline" size="sm" className="w-auto p-1">
+                        <a href={fileUrl} download>
+                            Zobraziť v plnej veľkosti
+                        </a>
+                    </Button>
+                </div>
+                <div className="flex justify-center items-center w-full h-full overflow-hidden">
                     {fileType === "image" ? (
                         <img 
                             src={fileUrl} 
                             alt={fileName}
                             className="max-w-full max-h-full object-contain"
-                            style={{ maxHeight: "80vh", width: "auto" }} // Ensure the image scales without exceeding the viewport
+                            style={{ maxHeight: "80vh", width: "auto" }}
                         />
                     ) : (
                         <iframe 
