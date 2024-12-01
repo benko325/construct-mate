15f65a65fe8d161d6a2471ca902757e552fac35b
Benjamín Havlík <benjamin.havlik@lifeliqe.com>
2024-12-02 00:45:30 +0100

    feat: added nice icons to the TopBar

--- frontend/src/features/TopBar.tsx
+++ frontend/src/features/TopBar.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../components/ui/button';
 import agent from '@/app/api/agent';
 import { useUser } from '@/context/UserContext';
+import { Home, User, LogOut } from "lucide-react";
 
 const TopBar: React.FC = () => {
     const { user, logout } = useUser();
@@ -32,20 +33,24 @@ const TopBar: React.FC = () => {
                 </h1>
                 <Link to="/dashboard">
                     <Button variant="outline">
+                        <Home className="w-5 h-5" />
                         Domov
                     </Button>
                 </Link>
             </div>
 
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center space-x-2">
                 <p><strong>Prihlásený uživateľ:</strong> {user?.name}</p>
                 <p><strong>Email:</strong> {user?.email}</p>
+                <div className="flex items-center space-x-1"></div>
                 <Link to="/profile">
                     <Button variant="outline">
+                    <User className="w-5 h-5" />
                         Môj profil
                     </Button>
                 </Link>
                 <Button onClick={handleLogout} variant="link" color="red">
+                    <LogOut className="w-5 h-5" />
                     Odhlásiť sa
                 </Button>
             </div>
